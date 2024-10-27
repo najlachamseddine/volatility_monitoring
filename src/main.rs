829f0c01f838d3dcb094dec5686774db431e37e0
najlachamseddine <najlachamseddine@yahoo.fr>
2024-10-27 18:10:44 +0000

    channel send/receive

--- src/main.rs
+++ src/main.rs
@@ -21,56 +21,28 @@ const WETH_USDC_POOL: &str = "0xC6962004f452bE9203591991D15f6b388e09E8D0"; // ge
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let (tx, mut rx): (Sender<f64>, Receiver<f64>) = mpsc::channel(32);
-
-    // let _res = fetch_uniswapv3_prices(WSS_URL, WETH_USDC_POOL, tx).await.expect("fetch uniswap prices");
-
-    // while let Some(value) = rx.recv().await {
-    //     println!("Dequeued: {}", value);
-    // }
-
+    
     let provider = Provider::<Ws>::connect(WSS_URL)
         .await
         .unwrap()
         .interval(Duration::from_millis(50u64));
     let client = Arc::new(provider);
-    println!("{:#?}", client);
-    let address = WETH_USDC_POOL.parse::<Address>().unwrap();
-    let uniswapv3 = UniswapV3Pool::new(address, Arc::clone(&client));
-    println!(">>>>>>>>>>>>>>>>> uniswapv3 address is {uniswapv3:?}");
-    let last_block = client.get_block_number().await.unwrap();
-    println!("{}", last_block);
+    // println!("{:#?}", client);
+    // let address = WETH_USDC_POOL.parse::<Address>().unwrap();
+    // let uniswapv3 = UniswapV3Pool::new(address, Arc::clone(&client));
+    // println!(">>>>>>>>>>>>>>>>> uniswapv3 address is {uniswapv3:?}");
+    // let last_block = client.get_block_number().await.unwrap();
+    // println!("{}", last_block);
 
-    // tokio::spawn(async move {
-    //     keep_connection_alive(client).await;
-    // });
+    tokio::spawn(async move {
+        keep_connection_alive(client).await;
+    });
 
-   
-    let events = uniswapv3.event::<SwapFilter>();
-    let mut stream = events.stream().await?;
-         while let Some(Ok(evt)) = timeout(Duration::from_secs(3600), stream.next())
-        .await
-        .unwrap()
-    {
-        println!("{:#?}", evt);
-        let _ = tx.send(2.0).await.expect("send my message ");
-    }
+    fetch_uniswapv3_prices(WSS_URL, WETH_USDC_POOL, tx).await?;
 
     while let Some(value) = rx.recv().await {
         println!("Dequeued: {}", value);
     }
-
-    // while let Some(Ok(evt)) = timeout(Duration::from_secs(360), stream.next()).await.unwrap() {
-    //     println!("{:#?}", evt);
-    //     // let _ = tx.send(2.0).await.expect("send my message ");
-    // }
-
-    //    while let Some(Ok(evt)) = timeout(Duration::from_secs(360), stream2.next())
-    //     .await
-    //     .unwrap()
-    // {
-    //     println!("{:#?}", evt);
-    //     // let _ = tx.send(2.0).await.expect("send my message ");
-    // }
 
     Ok(())
 }
--- src/on_chain.rs
+++ src/on_chain.rs
@@ -1,6 +1,8 @@
 use crate::uniswap_v3_pool::*;
 use ethers::{
-    contract::stream::EventStream, providers::{Middleware, Provider, StreamExt, Ws}, types::Address
+    contract::stream::EventStream,
+    providers::{Middleware, Provider, StreamExt, Ws},
+    types::Address,
 };
 use eyre::Result;
 use std::sync::Arc;
@@ -13,48 +15,29 @@ pub async fn fetch_uniswapv3_prices(
     tx: Sender<f64>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     println!("toto1");
-    let provider = Provider::<Ws>::connect(provider_url).await.unwrap().interval(Duration::from_millis(50u64));
+    let provider = Provider::<Ws>::connect(provider_url)
+        .await
+        .unwrap()
+        .interval(Duration::from_millis(50u64));
     let client = Arc::new(provider);
     println!("{:#?}", client);
     let address = pool_address.parse::<Address>()?;
     let uniswapv3 = UniswapV3Pool::new(address, Arc::clone(&client));
-    println!(">>>>>>>>>>>>>>>>> uniswapv3 address is {uniswapv3:?}");
     let last_block = client.get_block_number().await?;
-    println!("{}", last_block);
 
-    let s =  listen_specific_events(&uniswapv3).await.unwrap();
-
-    // let s = tokio::spawn(async move {
-    //     let events = uniswapv3.event::<SwapFilter>().from_block(last_block);
-    //     let event2 = uniswapv3.events();
-    //     let _stream1 = event2.stream().await.expect("get an event stream 1");
-    //     // println!("{:#?}", event);
-    //     let _stream2 = events.stream().await;
-    // })
-    // .await
-    // .unwrap();
-    // println!("toto2");
-    // while let Some(Ok(evt)) = timeout(Duration::from_secs(360), stream2.next())
-    //     .await
-    //     .unwrap_or(None)
-    // {
-    //     println!("{:#?}", evt);
-    //     let _ = tx.send(2.0).await.expect("send my message ");
-    // }
-
-    Ok(())
-}
-
-
-async fn listen_specific_events(contract: &UniswapV3Pool<Provider<Ws>>) -> Result<()> {
-    let events = contract.events();
-    let stream = events.stream().await.unwrap();
-
-    let _ = Box::new(stream);
-
-    // while let Some(Ok(f)) = stream.next().await {
-    //     println!("SwapFilter event: {f:?}");
-    // }
-
+    let _s = tokio::spawn(async move {
+        let events = uniswapv3.event::<SwapFilter>().from_block(last_block);
+        let mut stream = events.stream().await.unwrap();
+        println!("toto2");
+        while let Some(Ok(evt)) = timeout(Duration::from_secs(3600), stream.next())
+            .await
+            .unwrap_or(None)
+        {
+            println!("{:#?}", evt);
+            // tokio::spawn(async move {
+            let _ = tx.send(2.0).await.expect("send my message ");
+            // });
+        }
+    });
     Ok(())
 }
